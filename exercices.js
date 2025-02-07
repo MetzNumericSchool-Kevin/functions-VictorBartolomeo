76ed8099772126cd7b8ffc0c031bca95ead41710
VictorBartolomeo <victor.monteragioni@icloud.com>
2025-02-07 21:00:57 +0100

    Quel est le tarif d'une potion ?

--- exercices.js
+++ exercices.js
@@ -20,3 +20,16 @@ function salutations(nom_sorcier) {
 }
 
 console.log(salutations(nom_sorcier));
+
+// Quel est le tarif d'une potion ?
+
+function potionCost(potionID,inventaire,quantity=1){
+    const findPotion=inventaire.find(p=>p.id === potionID);
+    if(findPotion){
+        return findPotion.prix*quantity;
+    } else {
+        return "Je ne trouve pas la potion dans l'arrière boutique..."
+    }
+}
+
+console.log(potionCost("potion_soin",inventaire,3))
