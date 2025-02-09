0824d818fadc093d0392fd6ce4d1b366ff4632cc
VictorBartolomeo <victor.monteragioni@icloud.com>
2025-02-09 19:19:20 +0100

    Ajout de nouvelles potions dans l'inventaire

--- exercices.js
+++ exercices.js
@@ -23,30 +23,47 @@ console.log(salutations(nom_sorcier));
 
 // Quel est le tarif d'une potion ?
 
-function potionCost(potionID,inventaire,quantity=1){
-    const findPotion=inventaire.find(p=>p.id === potionID);
-    if(findPotion){
-        return findPotion.prix*quantity;
+function potionCost(potionID, inventaire, quantity = 1) {
+    const findPotion = inventaire.find(p => p.id === potionID);
+    if (findPotion) {
+        return findPotion.prix * quantity;
     } else {
         return "Je ne trouve pas la potion dans l'arrière boutique..."
     }
 }
 
-console.log(potionCost("potion_soin",inventaire,3))
+console.log(potionCost("potion_soin", inventaire, 3))
 
 // Fabrication de potion
 
-function craftPotion(potionID,prix=10,stock=1){
-    const findPotion=inventaire.find(p=>p.id === potionID);
-    if(!findPotion){
-        return inventaire.push({id:potionID,prix:prix,stock:stock});
-    }
-    else{
+function craftPotion(potionID, prix = 10, stock = 1) {
+    const findPotion = inventaire.find(p => p.id === potionID);
+    if (!findPotion) {
+        return inventaire.push({id: potionID, prix: prix, stock: stock});
+    } else {
         return console.error("Erreur, ce que vous fabriquez existe déjà.")
     }
 }
 
 craftPotion("potion_mana");
-craftPotion("potion_de_foyer",25,5);
-craftPotion("potion_de_magie",10);
+craftPotion("potion_de_foyer", 25, 5);
+craftPotion("potion_de_magie", 10);
+console.log(inventaire);
+
+// Ajout de nouvelles potions dans l'inventaire
+
+function addPotionToInventory(inventaire, potionID) {
+    const findPotion = inventaire.find(p => p.id === potionID.toLowerCase());
+    if (findPotion) {
+        findPotion.prix = parseInt(prompt("Changer le prix : "));
+        findPotion.stock = parseInt(prompt("Changer le stock : "));
+    } else {
+        craftPotion(
+            potionInput,
+            parseInt(prompt("Quel est le prix de la potion ?: ")),
+            parseInt(prompt("Combien en stock ?: "))
+        )
+    }
+}
+addPotionToInventory(inventaire, potionInput = prompt("Quel est le nom de ta potion ?").toLowerCase());
 console.log(inventaire);
